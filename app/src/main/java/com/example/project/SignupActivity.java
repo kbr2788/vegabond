6ecc9beb27cd0aa06cce0479a812e026771fc6d4
kbr2788 <kbr2788@naver.com>
2021-01-02 22:34:44 +0900

    page convert
    회원가입 -> 로그인 페이지 전환하기

--- app/src/main/java/com/example/project/SignupActivity.java
+++ app/src/main/java/com/example/project/SignupActivity.java
@@ -2,7 +2,11 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button imageButton = (Button) findViewById(R.id.button2);
+        imageButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
