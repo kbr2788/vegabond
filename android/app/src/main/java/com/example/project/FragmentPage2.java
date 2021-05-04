3441bb529f6ebad6a61ce9b15a8db40c99242f2d
kbr2788 <kbr2788@naver.com>
2021-05-04 20:57:51 +0900

    map

--- android/app/src/main/java/com/example/project/FragmentPage2.java
+++ android/app/src/main/java/com/example/project/FragmentPage2.java
@@ -13,7 +13,16 @@ import androidx.fragment.app.Fragment;
 
 public class FragmentPage2 extends Fragment {
 
-    private Button btnKangwon;
+    public Button btnKangwon;
+    public Button btnSudogwon;
+    public Button btnChungnam;
+    private Button btnChungbuk;
+    private Button btnGyeongbuk;
+    private Button btnGyeongnam;
+    private Button btnJeonbuk;
+    private Button btnJeonnam;
+    private Button btnJeju;
+    private Button btnDokdo;
 
     @Nullable
     @Override
@@ -21,9 +30,18 @@ public class FragmentPage2 extends Fragment {
         setHasOptionsMenu(true);
 
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_page_2,container,false);
-        btnKangwon = rootView.findViewById(R.id.btn_kangwon);
 
+        btnKangwon = rootView.findViewById(R.id.btn_kangwon);
         btnKangwon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), map.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSudogwon = rootView.findViewById(R.id.btn_sudogwon);
+        btnSudogwon.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
@@ -34,6 +52,7 @@ public class FragmentPage2 extends Fragment {
 
         return rootView;
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
--- android/app/src/main/java/com/example/project/map.java
+++ android/app/src/main/java/com/example/project/map.java
@@ -3,23 +3,33 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import net.daum.mf.map.api.CameraUpdate;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 public class map extends AppCompatActivity {
+
+    private FragmentPage2 frgp;
+    private Button btnKangwon;
+    private Button btnSudogwon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+
         MapView mapView = new MapView(this);
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);
 
         mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.829084, 128.148701), true);
         mapView.setZoomLevel(10, true);
+        mapViewContainer.addView(mapView);
     }
+
 }
